fa91889c73685fb77fab02d8266ccc7855767bb8
eppzx2 <elmirinfo351@gmail.com>
2025-11-21 11:04:17 +0000

    creating a class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-class Square: #defining a class
-    def __init__(self, size): #to start values
-        self.__size = size #size is private attribute so __ before it
+'''
+This module defines a square class
+'''
+class Square:
+    def __init__(self, size):
+        self.__size = size
 
 
